66e6b61253a868d306fb2d2ea6eb9aac33ce15d0
your.user.name <jeja2306@student.miun.se>
2025-12-02 15:39:15 +0100

    created connection to mongodb

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-import serve from "koa-static";
+/*import serve from "koa-static";
+app.use(serve("./dist"));*/
 
-app.use(serve("./dist"));
+"use strict";
 
-const Koa = require('koa');
+const Koa = require("koa");
+const Router = require("@koa/router");
+//const bodyParser = require("koa-bodyparser");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
+if (!process.env.MONGO_URI) {
+  console.error("MONGO_URI missing in .env");
+  process.exit(1);
+}
 
-app.listen(3000);
+// anslut till mongodb
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Ansluten till MongoDB");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error("Anslutningen misslyckas", err);
+    process.exit(1);
+  });
