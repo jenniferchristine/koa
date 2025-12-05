0524372234ef776669695803432a8fa6249c1ebd
your.user.name <jeja2306@student.miun.se>
2025-12-05 14:10:41 +0100

    added validations in mongoose schema

--- models/Book.js
+++ models/Book.js
@@ -4,9 +4,24 @@ const mongoose = require("mongoose");
 
 // skapa bookschema
 const bookSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    publication: { type: Number, required: true },
-    read: { type: Boolean, default: false }
+    title: { 
+        type: String, 
+        required: [true, "Titel måste anges"],
+        trim: true
+    },
+    publication: { 
+        type: Number, 
+        required: [true, "Utgivningsår måste anges"], 
+        min: [0, "Utgivningsår kan inte vara 0"],
+        validate: {
+            validator: Number.isInteger,
+            message: "Utgivningsår måste vara ett heltal"
+        }
+    },
+    read: { 
+        type: Boolean,
+        default: false 
+    }
 });
 
 module.exports = mongoose.model("Book", bookSchema);
