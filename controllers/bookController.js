9593a210594df73e0527fd612ae1531245a60d20
your.user.name <jeja2306@student.miun.se>
2025-12-05 14:20:16 +0100

    inserted validations to exports

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -4,7 +4,7 @@ const Book = require("../models/Book");
 const mongoose = require("mongoose");
 
 // funktion för mongoose validering
-function formatValidationErrors (err) {
+function formatValidationErrors(err) {
     const errors = {};
 
     for (let field in err.errors) {
@@ -17,6 +17,7 @@ function formatValidationErrors (err) {
 exports.getAllBooks = async (ctx) => {
     try {
         const books = await Book.find();
+
         ctx.body = books;
     } catch (err) {
         console.error("Error in getAllBooks: ", err);
@@ -48,10 +49,17 @@ exports.createBook = async (ctx) => {
     try {
         const newBook = new Book(ctx.request.body);
         const savedBook = await newBook.save();
+        ctx.status = 201;
+
         ctx.body = savedBook;
     } catch (err) {
-        console.error("Error in createBook: ", err);
-        ctx.throw(500, "Server error");
+        if (err.name === "ValidationError") {
+            ctx.status = 400;
+            ctx.body = { errors: formatValidationErrors(err) };
+        } else {
+            console.error("Error in createBook: ", err);
+            ctx.throw(500, "Server error");
+        }
     }
 };
 
@@ -61,16 +69,26 @@ exports.updateBook = async (ctx) => {
         const id = ctx.params.id;
 
         if (!mongoose.Types.ObjectId.isValid(id)) {
-            ctx.throw(400, "Invalid ObjectID");
+            ctx.throw(400, "Invalid ID");
         }
 
-        const updated = await Book.findByIdAndUpdate(id, ctx.request.body, { new: true });
+        const updated = await Book.findByIdAndUpdate(
+            id,
+            ctx.request.body,
+            { new: true, runValidators: true }
+        );
 
-        if (!updated) ctx.throw(404, "Book not updated");
+        if (!updated) ctx.throw(404, "Book could not update");
+
         ctx.body = updated;
     } catch (err) {
-        console.error("Error in updateBook: ", err);
-        ctx.throw(err.status || 500, err.message || "Server error");
+        if (err.name === "ValidationError") {
+            ctx.status = 400;
+            ctx.body = { errors: formatValidationErrors(err) };
+        } else {
+            console.error("Error in updateBook: ", err);
+            ctx.throw(err.status || 500, err.message || "Server error");
+        }
     }
 };
 
@@ -85,6 +103,7 @@ exports.deleteBook = async (ctx) => {
 
         const deleted = await Book.findByIdAndDelete(id);
         if (!deleted) ctx.throw(404, "Book not deleted");
+        
         ctx.body = { message: "Book deleted" };
     } catch (err) {
         console.error("Error in deleteBook: ", err);
